5c99afab8560cc087516cac2066e5dd49ab15c55
glen@stella <glen@accur8software.com>
2022-11-04 10:07:39 -0400

    interim commit

--- bootstrap/User.py
+++ bootstrap/User.py
@@ -74,7 +74,7 @@ class User(object):
     def installNix(self):
         if not os.path.exists("/nix"):
             self.execShell("""find /etc -name '*.backup-before-nix' | xargs rm -rf""")
-            installScript = f"{user.home}/temp-nix-install.sh"
+            installScript = f"{self.home}/temp-nix-install.sh"
             self.execShell(f"curl -L https://nixos.org/nix/install | sudo -u {user.name} tee {installScript} > /dev/null")
             user.execAsUser("sh", installScript, "--daemon")
             self.deleteFile(installScript)
